ec8db6fbdcc690e8f4e6c1442a4b61635270aed2
JesusC-R <cuevasrodartej@gmail.com>
2023-07-24 17:56:02 -0700

    Feat: Add Categories form.

--- app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
@@ -11,20 +11,20 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
       storeId: params.storeId,
     },
     orderBy: {
-      createdAt: "desc"
-    }
+      createdAt: "desc",
+    },
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedBillboards: BillboardColumn[] = billboards.map((item: any) => ({
     id: item.id,
     label: item.label,
-    createdAt: format(item.createdAt, "MMMM do, yyyy")
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={formattedBillboards}/>
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );
